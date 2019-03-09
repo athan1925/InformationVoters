0854c05ff15550491465600ca2e5a37d82c79475
Jonathan J. Dela Cruz <jjdelacruz@pup.edu.ph>
2019-03-09 20:45:00 +0800

    Add project files.

---
+++ VotersInfomationSystem/MYSQLCONNECTION.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace TeamJocas
+{
+    class MYSQLCONNECTION
+    {
+        public MySqlConnection conn;
+        public void Connection()
+        {
+            conn = new MySqlConnection("server = 192.168.0.10; user id = root; database = teamjocas");
+            // MYSQLCONNECTION conn = new MYSQLCONNECTION ();
+            // conn.Connection();
+        }
+    }
+}
